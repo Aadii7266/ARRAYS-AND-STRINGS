06e53f5b5ed3ce4e366c4c98d4e5eff84d1f69a5
Aadii7266 <adityaparihar7266@gmail.com>
2024-10-11 23:14:13 +0530

    Add files via upload

---
+++ concatenation.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string first_name,last_name;
+    first_name = "AADITYA";
+    last_name = "PARIHAR";
+
+    cout<<first_name+last_name;
+}
+
+/*
+OUTPUT: 
+JiyaPalod
+*/
